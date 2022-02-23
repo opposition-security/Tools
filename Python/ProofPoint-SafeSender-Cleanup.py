f92b4f1d055dc3e1b700502c5d5a7df376c42bd4
vosec <40548677+veilofsecurity@users.noreply.github.com>
2022-02-23 13:58:14 -0800

    Updated for naming and entries starting with @

--- Python/ProofPoint-SafeSender-Cleanup.py
+++ Python/ProofPoint-SafeSender-Cleanup.py
@@ -25,25 +25,27 @@ with open('userlist.csv', newline='') as csvfile:
     # parse each row in userlist csv
     for row in reader:
         # skip if the user doesn't have any safe sender entries
-        if row['whitelist'] != "":
+        if row['safelist'] != "":
             # create list of safe sender entries for user
-            safelist = row['whitelist'].split(";")
+            safelist = row['safelist'].split(";")
             # create blank list for email addresses
             newsafelist = []
 
             # iterate through each entry
             for entry in safelist:
                 # check if the entry is an email or domain
-                if "@" in entry:
+                if ("@" in entry) and not (entry.startswith('@')):
                     # if entry is an email, add to list for export
                     newsafelist.append(entry)
-                    
+                else:
+                    print(entry)
+
             # convert list to semicolon separated string
             newcsvsafelist = ';'.join(newsafelist)
             # write the filtered safe senders back to the csv row
-            row['whitelist'] = newcsvsafelist
+            row['safelist'] = newcsvsafelist
 
-        # write the row to the output file    
+        # write the row to the output file
         writer.writerow(row)
 
 # close output file
